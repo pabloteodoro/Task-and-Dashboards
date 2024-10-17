61a2c301820e17bf497c1ad2b4b8aac5f7d5b6c2
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-10-17 19:13:52 -0300

    import styles for main

--- projeto/src/pages/index.tsx
+++ projeto/src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.css';
+
 export default function Home() {
   return (
     
    
       <div>
-        <h1>NextJS</h1>
+        <Head>
+          <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
+        </Head>
+        <h1>Meu Projeto</h1>
       </div>
     
   );
