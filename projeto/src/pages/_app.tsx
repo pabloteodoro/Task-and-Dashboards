beee2da3359d6179ee4613315f4ac6a45aafdf33
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-10-20 13:56:08 -0300

    createAPI
    create news key from api google

--- projeto/src/pages/_app.tsx
+++ projeto/src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@/components/header';
 import '../styles/globals.css';
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react"
+import session from '../../node_modules/next-auth/core/routes/session.d';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
     <Header />
   <Component {...pageProps} />
-  </>
+  </SessionProvider>
   );
 }
