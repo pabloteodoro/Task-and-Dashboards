e31f4a466f2a762f2d8bab26f7207c0de4fd964c
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-10-21 18:59:13 -0300

    create security bypass
    I created a logic to block any type of bypass in which the user can access other pages without being logged into a session

--- projeto/src/pages/dashboard/index.tsx
+++ projeto/src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from 'next';
 import styles from './styles.module.css';
 import Head from 'next/head';
+import { getSession } from 'next-auth/react';
 
 export default function Dashboard() {
     return (
@@ -12,3 +14,21 @@ export default function Dashboard() {
         </div>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const session = await getSession({ req });
+    // console.log(session);
+
+    if (!session?.user) { // se não tiver logado vai redirecionar pra pagina inicial
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {},
+    };
+};
