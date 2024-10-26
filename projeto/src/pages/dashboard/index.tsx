e4209701625b6b8edb916b2791a42d7214a3822b
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-10-26 11:29:34 -0300

    box dashboard
    I organized the input box and styled it

--- projeto/src/pages/dashboard/index.tsx
+++ projeto/src/pages/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from 'next';
+import { useState } from 'react';
 import styles from './styles.module.css';
 import Head from 'next/head';
 import { getSession } from 'next-auth/react';
@@ -7,6 +8,9 @@ import { FiShare2 } from 'react-icons/fi';
 import { FaTrash } from 'react-icons/fa';
 
 export default function Dashboard() {
+    const [input, setInput] = useState("");
+
+
     return (
         <div className={styles.container}>
             <Head>
@@ -29,6 +33,25 @@ export default function Dashboard() {
                         </button>
                         </form>
                         </div>
+                </section>
+
+                <section className={styles.taskContainer}>
+                    <h1>Minhas Tarefas</h1>
+                    <article className={styles.task}>
+                        <div className={styles.tagContainer}>
+                            <label className={styles.tag}>PÚBLICO</label>
+                            <button className={styles.shareButton}>
+                              <FiShare2 size={22} color="#3183ff" />
+
+                            </button>
+                        </div>
+                        <div className={styles.taskContent}>
+                            <p>Minha primeira tarefa</p>
+                            <button className={styles.trashButton}>
+                                <FaTrash size={24} color="#ea3140" />
+                            </button>
+                        </div>
+                    </article>
                 </section>
             </main>
 
