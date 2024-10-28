08740a6336a695c67d9033c2b6213b31e8542068
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-10-28 19:16:32 -0300

    true or false?
    I created a whole logic to be able to connect with the bank and register the tasks so that they can be displayed whether they are public or not

--- projeto/src/pages/dashboard/index.tsx
+++ projeto/src/pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from 'next';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
 import styles from './styles.module.css';
 import Head from 'next/head';
 import { getSession } from 'next-auth/react';
@@ -8,7 +8,7 @@ import { FiShare2 } from 'react-icons/fi';
 import { FaTrash } from 'react-icons/fa';
 
 import { db } from '../../services/firebaseConnection'
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, query, orderBy, where, onSnapshot } from 'firebase/firestore';
 
 interface HomeProps {
     user: {
@@ -16,9 +16,51 @@ interface HomeProps {
     }
 }
 
+interface TaskProps {
+    id: string;
+    created: Date;
+    public: boolean,
+    tarefa: string;
+    user: string;
+}
+
 export default function Dashboard({user}: HomeProps) {
     const [input, setInput] = useState("")
     const [publicTask, setPublicTask] = useState(false);
+    const [task, setTask] = useState<TaskProps[]>([])
+ 
+    useEffect(() => {
+        async function loadTarefas() {
+
+            const tarefasRef = collection(db, 'tasks');
+            const q = query(
+                tarefasRef,
+                orderBy("created", "desc"),
+                where("user", "==", user?.email)
+            );
+
+            onSnapshot(q, (snapshot) => {
+                let lista = [] as TaskProps[];
+
+                snapshot.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        tarefa: doc.data().tarefa,
+                        created: doc.data().created,
+                        user: doc.data().user,
+                        public: doc.data().public
+
+                    });
+                });
+
+                setTask(lista);
+               
+            });
+
+        }
+
+        loadTarefas();
+    },[user?.email])
 
     function handleChangePublic (event: ChangeEvent<HTMLInputElement>) {
         setPublicTask(event.target.checked);
@@ -78,21 +120,28 @@ export default function Dashboard({user}: HomeProps) {
 
                 <section className={styles.taskContainer}>
                     <h1>Minhas Tarefas</h1>
-                    <article className={styles.task}>
-                        <div className={styles.tagContainer}>
-                            <label className={styles.tag}>PÚBLICO</label>
-                            <button className={styles.shareButton}>
-                              <FiShare2 size={22} color="#3183ff" />
 
-                            </button>
-                        </div>
-                        <div className={styles.taskContent}>
-                            <p>Minha primeira tarefa</p>
-                            <button className={styles.trashButton}>
-                                <FaTrash size={24} color="#ea3140" />
-                            </button>
-                        </div>
-                    </article>
+                   {task.map((item) => (
+                     <article key={item.id} className={styles.task}>
+                    {item.public && (
+                           <div className={styles.tagContainer}>
+                           <label className={styles.tag}>PÚBLICO</label>
+                           <button className={styles.shareButton}>
+                             <FiShare2 size={22} color="#3183ff" />
+  
+                           </button>
+                       </div>
+                    )}
+                     <div className={styles.taskContent}>
+                         <p>{item.tarefa}</p>
+                         <button className={styles.trashButton}>
+                             <FaTrash size={24} color="#ea3140" />
+                         </button>
+                     </div>
+                 </article>
+
+                   ))}
+
                 </section>
             </main>
 
