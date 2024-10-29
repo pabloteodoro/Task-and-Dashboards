003f97c4e9e32fed5b434fb3c980825158655b60
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-10-29 19:01:28 -0300

    new feature in dashboard page
    I created a new feature that gives functionality to the trash and share buttons

--- projeto/src/pages/dashboard/index.tsx
+++ projeto/src/pages/dashboard/index.tsx
@@ -8,7 +8,8 @@ import { FiShare2 } from 'react-icons/fi';
 import { FaTrash } from 'react-icons/fa';
 
 import { db } from '../../services/firebaseConnection'
-import { addDoc, collection, query, orderBy, where, onSnapshot } from 'firebase/firestore';
+import { addDoc, collection, query, orderBy, where, onSnapshot, doc, deleteDoc } from 'firebase/firestore';
+import Link from 'next/link';
 
 interface HomeProps {
     user: {
@@ -89,6 +90,18 @@ export default function Dashboard({user}: HomeProps) {
         }
     }
 
+    async function handleShare(id: string) {
+        await navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_URL}/task/${id}`);
+
+        alert("URL Copiada com sucesso!");
+    }
+
+
+    async function handleDeleteTask(id: string) {
+        const docRef = doc(db, 'tasks', id);
+        await deleteDoc(docRef);
+    }
+
 
     return (
         <div className={styles.container}>
@@ -126,28 +139,33 @@ export default function Dashboard({user}: HomeProps) {
                     {item.public && (
                            <div className={styles.tagContainer}>
                            <label className={styles.tag}>PÚBLICO</label>
-                           <button className={styles.shareButton}>
-                             <FiShare2 size={22} color="#3183ff" />
-  
-                           </button>
-                       </div>
-                    )}
-                     <div className={styles.taskContent}>
-                         <p>{item.tarefa}</p>
-                         <button className={styles.trashButton}>
-                             <FaTrash size={24} color="#ea3140" />
-                         </button>
-                     </div>
-                 </article>
+                       <button className={styles.shareButton} onClick={() => handleShare(item.id)}>
+                         <FiShare2 size={22} color="#3183ff" />
+                       </button>
+                   </div>
+                )}
+                
+                 <div className={styles.taskContent}>
+                     
+                     {item.public ? (
+                        <Link href={`/task/${item.id}`}>
+                        <p>{item.tarefa}</p>
+                        </Link>
+                     ) : (
+                        <p>{item.tarefa}</p>
+                     )}
 
-                   ))}
+                     <button className={styles.trashButton} onClick={() => handleDeleteTask(item.id)}>
+                         <FaTrash size={24} color="#ea3140" />
+                     </button>
+                 </div>
+             </article>
+               ))}
 
-                </section>
-            </main>
-
-           
-        </div>
-    );
+            </section>
+        </main>
+    </div>
+);
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
