40291ffecbdacc09e1c212b17ee05529b62f53e4
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-10-30 19:12:11 -0300

    details of task
    I adjusted the backend so that it can bring me information about the details of the tasks that the user created

---
+++ projeto/src/pages/task/[id].tsx
@@ -0,0 +1,62 @@
+import Head from "next/head";
+import styles from './styles.module.css'
+import { GetServerSideProps } from "next";
+import { db } from '../../services/firebaseConnection'
+import { doc, collection, query, where, getDoc } from 'firebase/firestore' 
+
+export default function Task() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Detalhes da Tarefa</title>
+            </Head>
+
+            <main className={styles.main}>
+                <h1>Detalhes da Tarefa</h1>
+            </main>
+            
+        </div>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    const id = params?.id as string;
+
+    const docRef = doc(db, 'tasks', id);
+
+    const snapshot = await getDoc(docRef);
+
+    if (snapshot.data() === undefined) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    if (!snapshot.data()?.public) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    const miliseconds = snapshot.data()?.created.seconds * 1000;
+
+    const task = {
+        tarefa: snapshot.data()?.tarefa,
+        public: snapshot.data()?.public,
+        created: new Date(miliseconds).toLocaleDateString(),
+        user: snapshot.data()?.user,
+        taskId: id,
+    }
+
+    console.log(task);
+
+    return {
+        props: {},
+    };
+};
